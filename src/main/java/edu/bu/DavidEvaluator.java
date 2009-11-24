38a9eb6ec79f9a6b54b1666f9bf2c1e3eccd2c57
David LaPalomento <dml@eta.(none)>
2009-11-23 20:24:54 -0500

    extra players

--- src/main/java/edu/bu/DavidEvaluator.java
+++ src/main/java/edu/bu/DavidEvaluator.java
@@ -3,6 +3,9 @@
  */
 package edu.bu;
 
+import java.util.EnumMap;
+import java.util.HashMap;
+
 /**
  * @author dml
  *
@@ -14,7 +17,18 @@ public class DavidEvaluator implements Evaluator {
 	 */
 	@Override
 	public int evaluateMove(AtroposState currentstate) {
-		// TODO Auto-generated method stub
+		if (currentstate.isFinished()) {
+			return Integer.MIN_VALUE;
+		}
+		EnumMap<Colors, Colors> pairs = new EnumMap<Colors, Colors>(
+				Colors.class);
+		int top = Math.min(currentstate.lastPlay.height(), currentstate.circles.length - 3);
+		int left = Math.min(currentstate.lastPlay.leftDistance(), currentstate.circles.length - 3);
+		for(int h = 0; h < 2; ++h) {
+			for (int l = 0; l < 2; ++l) {
+				pairs.put(Colors.values()[currentstate.circles[top + h][left + l].getColor()], Colors.Uncolored);
+			}
+		}
 		return 0;
 	}
 
--- src/main/java/edu/bu/JustinPlayer.java
+++ src/main/java/edu/bu/JustinPlayer.java
@@ -3,25 +3,20 @@ package edu.bu;
 import java.io.IOException;
 import java.io.StringReader;
 
+public class JustinPlayer {
 
-/**
- * Main class for {@link Player}.
- * 
- */
-public class DeepPlayer {
 	/**
 	 * @param args
-	 *            - the command-line arguments
-	 * @throws IOException
-	 *             - if there is an error processing the command-line arguments
+	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
 		AtroposState state = new AtroposStateReader(new StringReader(args[0])).read();
 		
 		// Calculate next move
-		Player player = new Player(7, new DefensiveEvaluator());
+		Player player = new Player(7, new JustinEvaluator());
 		AtroposState newstate = player.makeMove(state);
 		
 		System.out.println(newstate.lastPlay.getColorLocationString());
 	}
+
 }
